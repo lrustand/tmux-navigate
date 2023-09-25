9bb4097b73c85fd943225e5827acb8ed477384d2
Lars Rustand <rustand.lars@gmail.com>
2023-09-25 22:38:48 +0200

    Add emacs plugin

---
+++ tmux-navigate.el
@@ -0,0 +1,23 @@
+-*- lexical-binding: t; -*-
+
+(defun tmux-navigate-directions ()
+  (let* ((x (nth 0 (window-edges)))
+         (y (nth 1 (window-edges)))
+         (w (nth 2 (window-edges)))
+         (h (nth 3 (window-edges)))
+
+         (can_go_up (> y 2))
+         (can_go_down (<  (+ y h) (- (frame-height) 2)))
+         (can_go_left (> x 1))
+         (can_go_right (< (+ x w) (frame-width))))
+
+    (send-string-to-terminal
+     (format "\e]2;emacs %s #%s\a"
+	(buffer-name)
+        (string
+	  (if can_go_up    ?U 1)
+          (if can_go_down  ?D 1)
+          (if can_go_left  ?L 1)
+          (if can_go_right ?R 1))))))
+
+(add-hook 'buffer-list-update-hook 'tmux-navigate-directions)
